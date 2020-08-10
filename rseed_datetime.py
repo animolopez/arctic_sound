62a206d222451a9e1f651ed797e3ea4121b45f20
animolopez <eguchi.hikaru.kyushu@gmail.com>
2020-08-10 17:05:47 +0900

    update

--- rseed_datetime.py
+++ rseed_datetime.py
@@ -6,23 +6,21 @@ import os
 
 # Get current time with class 'int'
 dt_now = datetime.datetime.now()
-print(dt_now)
+#print(dt_now)
 dt_str = datetime.datetime.strftime(dt_now, '%Y/%m/%d %H:%M')
-print(dt_str)
+#print(dt_str)
 time_str = dt_now.strftime('%H%M') # get current time (HHMM, class 'str')
-print(time_str)
-print(type(time_str))
+#print(time_str)
+#print(type(time_str))
 time_int = int(time_str) # current time (HHMM, class 'int')
-print(time_int)
-print(type(time_int))
+#print(time_int)
+#print(type(time_int))
 
 # Record and export logs of a random seed
 output_dir = 'C:\\Users\\hikar\\Documents\\python\\test\\'
 logfile_name = 'timelog.txt'
-#os.chdir(output_dir)
-#print(os.getcwd())
 path = output_dir + logfile_name
-path
+
 if os.path.isfile(path) is True:
     with open(path, mode='a') as log:
         log.write('\nThe random seed is: %s' % time_str)
